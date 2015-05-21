01a583d6e0cf51426b2b647f5df0d985907834f8
easyTsai <easyboy543@gmail.com>
2015-05-21 15:57:36 +0800

    Add get json data funcion.

--- addr.php
+++ addr.php
@@ -4,10 +4,16 @@
 
     $db = new DB();
     $db->connect_db($_DB['host'], $_DB['username'], $_DB['password'], $_DB['dbname']);
-    $db->query("SELECT COL 4, COL 5 from tbl_name");
+    $db->query("SELECT * from tbl_name");
+	$first = 1;
     while($result = $db->fetch_array())
     {
-		
+		if($first){
+			$first = 0;
+			continue;
+		}
+		echo json_encode($result);
+		echo "<br />";
     }
     
 
